0693c2536123bb21398b92154681509492ebdd02
mishmesh <68383329+Michael-Boethas@users.noreply.github.com>
2024-11-10 19:41:11 +0100

    Added OG meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,30 +17,32 @@ export default function RootLayout({ children }) {
         <meta charSet="utf-8" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Web Developer Portfolio" />
+        <meta property="og:description" content="Portfolio of a web developer specializing in front-end and back-end technologies like React and Express." />
+        <meta property="og:image" content="https://mishmesh-portfolio.vercel.app/opengraph-image.png?v=2" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mishmesh-portfolio.vercel.app" />
+
+
+        {/* Twitter Cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="/opengraph-image.png" />
+
+
+        {/* Icons */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="48x48"
-          href="/favicon-48x48.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link
-          rel="icon"
-          sizes="192x192"
-          href="/web-app-manifest-192x192.png"
-        />
-        <link
-          rel="icon"
-          sizes="512x512"
-          href="/web-app-manifest-512x512.png"
-        />
+        <link rel="icon" sizes="192x192" href="/web-app-manifest-192x192.png" />
+        <link rel="icon" sizes="512x512" href="/web-app-manifest-512x512.png" />
 
         {/* Devicon CDN */}
         <link
@@ -49,11 +51,9 @@ export default function RootLayout({ children }) {
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
         />
 
+        {/* Mobile Web App */}
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-title" content={metadata.title} />
       </head>
       <LanguageProvider>
@@ -74,7 +74,6 @@ export default function RootLayout({ children }) {
           </body>
         </ThemeProvider>
       </LanguageProvider>
-
     </html>
   );
 }
