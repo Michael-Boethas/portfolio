b0605f1fd97c152fa1fabef26e95f8d6d2b5e93b
Michael-Boethas <68383329+Michael-Boethas@users.noreply.github.com>
2024-12-27 15:02:19 +0100

    Added dynamic setting of the color theme based on user preference

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -2,12 +2,18 @@
 
 'use client';
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('light'); // Default theme
+
+  // Set theme based on user preference
+  useEffect(() => {
+    const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    setTheme(preferredTheme);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
