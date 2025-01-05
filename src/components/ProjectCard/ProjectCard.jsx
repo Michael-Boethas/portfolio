0c5c0547cda5b4ece76239bdbf2a76e2016e735e
Michael-Boethas <68383329+Michael-Boethas@users.noreply.github.com>
2025-01-05 15:32:12 +0100

    Added project type tags

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -11,6 +11,20 @@ export default function ProjectCard({ projectData }) {
   const { theme } = useTheme();
   const [isCollapsed, setIsCollapsed] = useState(true);
 
+  const themePrefix = theme === 'light' ? 'theme-L-' : 'theme-D-';
+  let tagColor = '';
+
+  switch (projectData.type) {
+    case 'frontend':
+      tagColor = themePrefix + 'frontend-tag';
+      break;
+    case 'backend':
+      tagColor = themePrefix + 'backend-tag';
+      break;
+    default:
+      tagColor = themePrefix + 'fullstack-tag';
+  }
+
   const toggleContent = () => {
     setIsCollapsed(!isCollapsed);
   };
@@ -25,62 +39,69 @@ export default function ProjectCard({ projectData }) {
 
   return (
     <figure
-      className={`project-card hover--zoom card shadow rounded-0 ${theme === 'light' ? 'theme-L-bg-project-card' : 'theme-D-bg-project-card text-white'}`}
+      className={`project-card hover--zoom card shadow rounded-0 flex-row ${theme === 'light' ? 'theme-L-bg-project-card' : 'theme-D-bg-project-card text-white'}`}
       tabIndex={0}
       onClick={toggleContent}
       onKeyDown={keyDownClick}
       role="button"
       aria-expanded={!isCollapsed}
     >
-      <Image
-        className="project-card__image"
-        src={projectData.thumbnail_url}
-        alt={`Screenshot of ${projectData.name}`}
-        width={400}
-        height={300}
-        priority
-      />
-      <figcaption className="card-body">
-        <div className="d-flex gap-2 justify-content-between align-items-center">
-          <h3 className="card-title">{projectData.name}</h3>
-          <div className="d-flex gap-2">
-            {projectData.stack.map((tech, index) => (
-              <div key={index} className="fs-2">
-                <span
-                  className={`${theme === 'light' ? '' : 'dark-icon-glow'} ${techStack[tech].icon_light}`}
-                ></span>
-              </div>
-            ))}
+      <div className={` ${isCollapsed ? '' : ''} ${tagColor} text-white d-flex flex-column p-2`}>
+        {projectData.type.split('').map((letter, index) => {
+          return <span className='fst-italic fw-bold' key={index}>{letter.toUpperCase()}</span>;
+        })}
+      </div>
+      <div className="">
+        <Image
+          className="project-card__image"
+          src={projectData.thumbnail_url}
+          alt={`Screenshot of ${projectData.name}`}
+          width={400}
+          height={300}
+          priority
+        />
+        <figcaption className="card-body">
+          <div className="d-flex gap-2 justify-content-between align-items-center">
+            <h3 className="card-title">{projectData.name}</h3>
+            <div className="d-flex gap-2">
+              {projectData.stack.map((tech, index) => (
+                <div key={index} className="fs-2">
+                  <span
+                    className={`${theme === 'light' ? '' : 'dark-icon-glow'} ${techStack[tech].icon_light}`}
+                  ></span>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
 
-        <div
-          className={`card-text collapse p-2 border-top ${isCollapsed ? '' : 'show'}`}
-        >
-          <p>{projectData.description}</p>
+          <div
+            className={`card-text collapse p-2 border-top ${isCollapsed ? '' : 'show'}`}
+          >
+            <p>{projectData.description}</p>
 
-          <div className="d-flex flex-column gap-2 py-3">
-            <a
-              href={projectData.project_url}
-              className={`btn btn-primary rounded-0 ${theme === 'light' ? '' : 'text-white'}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              onKeyDown={keyDownClick}
-            >
-              {language === 'fr' ? 'Projet' : 'Project'}
-            </a>
-            <a
-              href={projectData.codebase_url}
-              className={`btn btn-primary rounded-0 ${theme === 'light' ? '' : 'text-white'}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              onKeyDown={keyDownClick}
-            >
-              Code
-            </a>
+            <div className="d-flex flex-column gap-2 py-3">
+              <a
+                href={projectData.project_url}
+                className={`btn btn-primary rounded-0 ${theme === 'light' ? '' : 'text-white'}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onKeyDown={keyDownClick}
+              >
+                {language === 'fr' ? 'Projet' : 'Project'}
+              </a>
+              <a
+                href={projectData.codebase_url}
+                className={`btn btn-primary rounded-0 ${theme === 'light' ? '' : 'text-white'}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onKeyDown={keyDownClick}
+              >
+                Code
+              </a>
+            </div>
           </div>
-        </div>
-      </figcaption>
+        </figcaption>
+      </div>
     </figure>
   );
 }
